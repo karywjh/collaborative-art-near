77715ed80beba848ee9319c6fe1184b9e4cbfeaf
Aego <aego.yu@gmail.com>
2022-11-20 16:00:44 -0600

    initialized parcel web

---
+++ web/src/pages/Launch.tsx
@@ -0,0 +1,7 @@
+import styles from './Launch.module.scss'
+
+const Launch = () => {
+  return <div className={styles.page}></div>
+}
+
+export default Launch
---
+++ web/src/router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Launch from './pages/Launch'
+
+const router = createBrowserRouter([{ path: '/', element: <Launch /> }])
+
+export default router
